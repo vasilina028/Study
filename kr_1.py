466b5d2028585a676f052dfe09c7eee3595b6905
vasilina028 <vasilina028@gmail.com>
2025-03-05 17:29:44 +0300

    Add files via upload

--- kr_1.py
+++ kr_1.py
@@ -2,10 +2,10 @@ num1 = int(input())
 num2 = int(input())
 num3 = int(input())
 num4 = int(input())
-for i in range(num1, num2+1):
-        for j in range(num3, num4+1):
-            s = j*i
-            print(s)
+for i_num in range(num1, num2+1):
+        for j_num in range(num3, num4+1):
+            result = j_num*i_num
+            print(result)
 
 
 
