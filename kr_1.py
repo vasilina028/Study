d606c3a2d0a965924efd27f3b5b059f9253c48f7
vasilina028 <vasilina028@gmail.com>
2025-03-05 16:55:14 +0300

    Add files via upload
    правильный файл

---
+++ kr_1.py
@@ -0,0 +1,12 @@
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+num4 = int(input())
+for i in range(num1, num2+1):
+        for j in range(num3, num4+1):
+            s = j*i
+            print(s)
+
+
+
+
